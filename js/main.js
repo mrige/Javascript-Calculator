53e98ab4cdc2d13f376a2ec8f1b1f59e4fe9b350
Ayobami Ige <ayobamiige@Ayobamis-MacBook-Pro.local>
2017-08-21 15:29:29 -0500

    Updated the subtract function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 var inputval = "";
 var digits = [];
+var entries = 0;
 var result = 0.0;
 var opPressed = false;
 
@@ -9,6 +10,7 @@ function display(id){
     const val = document.getElementById(id).value;
     inputval = inputval + val;
     document.getElementById("nameinput").value = inputval;
+    digits.push(parseFloat(inputval));
     console.log("IN");
 }
 
@@ -17,22 +19,35 @@ function reset(){
     document.getElementById("nameinput").value = inputval;
     result = 0.0;
     digits = [];
+    entries = 0;
 }
 
 function add() {
 
-    var val = document.getElementById("nameinput").value;
-    digits.push(parseFloat(val));
+   // var val = document.getElementById("nameinput").value;
+    //digits.push(parseFloat(val));
     inputval = "";
     result += digits.pop();
+    entries++;
     document.getElementById("nameinput").value = result;
+    digits.push(result);
+    console.log(result);
 }
 
 function subtract(){
-    var val = document.getElementById("nameinput").value;
-    digits.push(parseFloat(val));
+   // var val = document.getElementById("nameinput").value;
+    //digits.push(parseFloat(val));
+    console.log(result);
     inputval = "";
-    result -= digits.pop();
+    if(entries == 0){
+        result = digits.pop();
+    }
+    else {
+        result -= digits.pop();
+    }
+    entries++;
+    console.log(entries);
+
     document.getElementById("nameinput").value = result;
 }
 
